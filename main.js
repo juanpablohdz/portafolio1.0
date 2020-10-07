62c0ff6e97bd8a6c1202b854ed160323177b74f8
juanpablohdz <juan.hdz9901@gmail.com>
2020-10-07 13:07:29 -0500

    portafolio1.0

---
+++ main.js
@@ -0,0 +1,69 @@
+const grid = new Muuri('.grid', {
+
+    layout: {
+
+        rounding: false
+    }
+
+
+
+});
+
+/*funcion  */
+window.addEventListener('load', () => {
+    grid.refreshItems().layout();
+    document.getElementById('grid').classList.add('imagenes-cargadas');
+
+    const enlaces = document.querySelectorAll('#categorias a');
+    enlaces.forEach((elemento) => {
+
+        elemento.addEventListener('click', (evento) => {
+            /*comprtamiento determinado de la pagina */
+            evento.preventDefault();
+            enlaces.forEach((enlace) => enlace.classList.remove('activo'));
+            evento.target.classList.add('activo');
+
+            const categoria = evento.target.innerHTML.toLowerCase();
+            /*filtrado por categoria ----->*/
+            categoria === 'inicio' ? grid.filter('[data-categoria]') : grid.filter(`[data-categoria="${categoria}"]`);
+
+
+        });
+    });
+
+
+    //agregar  listener
+    const overlay = document.getElementById('overlay');
+    document.querySelectorAll('.grid .item img').forEach((elemento) => {
+
+
+
+        elemento.addEventListener('click', () => {
+            const ruta = elemento.getAttribute('src');
+            const descripcion = elemento.parentNode.parentNode.dataset.descripcion;
+
+            overlay.classList.add('activo');
+            document.querySelector('#overlay img').src = ruta;
+            document.querySelector('#overlay .descripcion').innerHTML = descripcion;
+        });
+    });
+
+
+    //event lisener boton cerrar
+    document.querySelector('#btn-cerrar-popup').addEventListener('click', () => {
+        overlay.classList.remove('activo');
+
+    });
+
+    //eventlistener del overlay
+    overlay.addEventListener('click', (evento) => {
+
+        //;
+
+        evento.target.id === 'overlay' ? overlay.classList.remove('activo') : '';
+
+    });
+
+
+
+});
